2c3e4752f9ddce36b0ac2935f57da4eeee07fd77
arjansunar <arjan.gahatrajsunar@gmail.com>
2024-01-28 12:12:36 +0545

    feat: add form actions

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -4,7 +4,7 @@
     name: string;
     done: boolean;
   };
-  let todoList: Todo[] = [];
+  let todos: Todo[] = [];
   function handleSubmit(e: SubmitEvent) {
     e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
@@ -13,7 +13,14 @@
       return;
     }
 
-    todoList = [...todoList, { id: crypto.randomUUID(), name, done: false }];
+    todos = [...todos, { id: crypto.randomUUID(), name, done: false }];
+  }
+
+  function deleteTodo(id: string) {
+    todos = todos.filter((t) => t.id !== id);
+  }
+  function toggleTodo(id: string) {
+    todos = todos.map((t) => (t.id === id ? { ...t, done: !t.done } : t));
   }
 </script>
 
@@ -29,7 +36,7 @@
     <button type="submit">Add</button>
   </form>
   <div class="flex flex-col gap-2 pt-10">
-    {#each todoList as todo}
+    {#each todos as todo}
       <!-- content here -->
       <li class="flex justify-between items-center min-w-[50vw]">
         <h2 class={`${todo.done ? "line-through" : ""}`}>
@@ -37,8 +44,16 @@
           {todo.name}
         </h2>
         <div class="flex gap-2 items-center">
-          <input type="checkbox" class="checkbox" checked={todo.done} />
-          <button class="btn btn-error btn-sm">Delete</button>
+          <input
+            type="checkbox"
+            class="checkbox"
+            checked={todo.done}
+            on:change={() => toggleTodo(todo.id)}
+          />
+          <button
+            class="btn btn-error btn-sm"
+            on:click={() => deleteTodo(todo.id)}>Delete</button
+          >
         </div>
       </li>
     {/each}
