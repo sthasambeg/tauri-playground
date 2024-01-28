2d994bddf4a26328423ef638fae386ab87c67aa5
Sam Stha <sthasam2@gmail.com>
2024-01-28 11:47:15 +0545

    feat: init svelte with skeleton

---
+++ svelte/src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  function HelloWorld() {
+    return "Hello Meow World!";
+  }
+</script>
+
+<main class="container">
+  <header>
+    <h1 class="text-lg">
+      {HelloWorld()}
+    </h1>
+  </header>
+</main>
+
+<style>
+</style>
